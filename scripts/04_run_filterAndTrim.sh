77b9b6b7b2dd78080463ee57133155b6b1dac471
ggouws <136603000+ggouws@users.noreply.github.com>
2026-01-30 17:24:12 +0000

    Update 04_run_filterAndTrim.sh
    Corrected environment

--- scripts/04_run_filterAndTrim.sh
+++ scripts/04_run_filterAndTrim.sh
@@ -73,5 +73,5 @@ if [ "$marker" ]; then ARGS="$ARGS -C $marker"; fi
 
 ## load R and call Rscript
 source ~/.bash_profile
-conda activate /mnt/community/Genomics/apps/miniforge/miniforge3/envs/metabarcoding
+conda activate metabarcoding
 Rscript $PWD/scripts/04_filterAndTrim.R $ARGS
